721eb172134d8cd1f0169c71c5c5d1ea13e17f54
Aiden <goldapple820@gmail.com>
2023-06-08 11:03:27 +0900

    render each Teletubby card

--- src/pages/Teletubbies/index.js
+++ src/pages/Teletubbies/index.js
@@ -18,6 +18,29 @@ const Teletubbies = () => {
     getTeletubbies();
   }, []);
 
+  //Render each Teletubby card
+  const renderTeletubbyCard = (teletubby, index) => {
+    return (
+      <Grid item xs={12} sm={12} md={12} lg={12} xl={12} key={index}>
+        <img
+          src={teletubby.image}
+          alt={teletubby.name}
+          style={{ width: "50%" }}
+        />
+        <div style={{ padding: "10px" }}>
+          <h3>{teletubby.name}</h3>
+          <p>{teletubby.description}</p>
+          <div>
+            {teletubby.traits.map((trait, i) => (
+              <button key={i} style={{ margin: "5px" }}>
+                {trait}
+              </button>
+            ))}
+          </div>
+        </div>
+      </Grid>
+    );
+  };
   
 }
 
